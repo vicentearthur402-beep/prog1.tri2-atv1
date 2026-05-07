1e582735c4ca6f9bc8d3ddab3a8a09c44f2c146f
Your Name <you@example.com>
2026-05-07 09:49:50 -0300

    recriado repositorio

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  routes: {},
+  async fetch(req) {
+    const url = new URL(req.url);
+    const pathname = url.pathname;
+    const localPath = (pathname === "/") 
+      ? `./public/index.html`
+      : `./public${pathname}`;
+    const file = Bun.file(localPath);
+    const exists = await file.exists();
+    if (!exists)
+      return new Response("Not Found", { status: 404 });
+    return new Response(file);
+  },
+});
+console.log(`Server running at ${server.url}`);
