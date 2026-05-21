ce5b42a8b75042da820ea562540b7e285bb00c21
Your Name <you@example.com>
2026-05-21 09:55:45 -0300

    Criado comando add do CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,36 @@
 // Command Line Interface
 
+// import { Item, TodoList } from "./core"
+// import { Item as Item1, TodoList as TodoList1 } from "./core";
+
+import TodoListClass, { Item } from "./core";
+
+const todoList = new TodoListClass("todolist.json")
+
 const params = process.argv;
 const command = params[2];
 
 if (command === "add") {
     const value = params[3];
-    console.log("Aqui vai a lógica de adicionar: " + value);
+    if (!value) {
+        console.error("Valor do item é obrigatório");
+        process.exit(1);
+    }
+    try {
+    await todoList.addItem(new Item(value))
+    } catch (error) {
+        console.error("Erro ao adicionar itens ", error)
+    }
+
+    console.log(`Item "${value}" adicionado com sucesso`)
     process.exit(0);
 }
 
- console.log("não entrou em if nenhum")
+ console.log(`Comando não reconhecido ${command}
+    Comandos Disponíveis:
+    - add <item>: Adiciona um item a lista
+    - remove <index>: Remove um item da lista por índice
+    - list: Lista os itens atuais
+    
+    
+    `)
--- src/core.ts
+++ src/core.ts
@@ -1,5 +1,5 @@
 class Item {
-    constructor(public item: string) { }
+    constructor(public title: string) { }
 }
 
 class TodoList {
@@ -10,31 +10,41 @@ class TodoList {
         this.filePath = filePath;
     }
 
-    addItem(item: Item) {
-        this.items.push(item);
+    private async saveListToDisk() {
+        const file = Bun.file(this.filePath);
+        const data = JSON.stringify(this.items);
+        await file.write(data)
+
     }
 
-    removeItem(index: number) {
+    private async loadListFromDisk() {
+        const file = Bun.file(this.filePath);
+        const data = await file.json();
+        this.items = data.map((v: any) => new Item(v.title));
+    }
+
+    async addItem(item: Item) {
+        if (!item)
+            throw "Item Inválido";
+        if (!item.title.trim())
+            throw "Item deve conter um título"
+        this.items.push(item);
+        await this.saveListToDisk();
+    }
+
+    async removeItem(index: number) {
         this.items.splice(index, 1);
+        await this.saveListToDisk();
 
     }
  
     getItems() {
-        return this.items;
+        return Array.from(this.items);
     }
 
 
 }
 
-const lista = new TodoList('arquivo.txt')
 
-lista.addItem(new Item("Comprar Abacate"))
-lista.addItem(new Item("Comprar Abacate"))
-lista.addItem(new Item("Comprar Abacate"))
-lista.addItem(new Item("Comprar Abacate"))
-lista.addItem(new Item("Comprar Abacate"))
-lista.addItem(new Item("Comprar Abacate"))
-lista.addItem(new Item("Comprar Abacate"))
-lista.addItem(new Item("Comprar Abacate"))
-
-console.log(lista.getItems())
+export default TodoList;
+export { TodoList, Item }
